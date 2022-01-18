1fed432b87475a066ab1d0349d887c74d355682a
Noah <yuenuni@hotmail.com>
2022-01-18 11:22:24 +0800

    add env.port

--- gqlServer.js
+++ gqlServer.js
@@ -79,7 +79,7 @@ const LunchServer = async () => {
     useUnifiedTopology: true,
   })
 
-  const PORT = 5000
+  const PORT = process.env.PORT || 5000
   httpServer.listen(PORT, () =>
     console.log(`Server is now running on http://localhost:${PORT}/graphql`)
   )
