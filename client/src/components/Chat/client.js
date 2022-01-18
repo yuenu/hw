9046d90be912b44ff8201893177ff4f043e98857
Noah <yuenuni@hotmail.com>
2022-01-18 14:57:08 +0800

    link settings

--- client/src/components/Chat/client.js
+++ client/src/components/Chat/client.js
@@ -2,15 +2,16 @@ import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { WebSocketLink } from '@apollo/client/link/ws'
 
-const websocketLink = process.env.NODE_ENV === 'development' ? `ws://localhost:${process.env.REACT_APP_PORT || 5000}/subscriptions` : `wss://realtime-chat-with-yuenu.herokuapp.com/subscriptions`
+const websocketLinkSet = process.env.NODE_ENV === 'development' ? `ws://localhost:${process.env.REACT_APP_PORT || 5000}/subscriptions` : `wss://realtime-chat-with-yuenu.herokuapp.com/subscriptions`
+const httpLinkSet = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.REACT_APP_PORT || 5000}/graphql` : 'https://realtime-chat-with-yuenu.herokuapp.com/graphql'
 
 console.log(process.env.NODE_ENV)
 const httpLink = new HttpLink({
-  uri: `http://localhost:${process.env.REACT_APP_PORT || 5000}/graphql`,
+  uri: httpLinkSet,
 })
 
 const wsLink = new WebSocketLink({
-  uri: websocketLink,
+  uri: websocketLinkSet,
   options: {
     reconnect: true,
   },
--- gqlServer.js
+++ gqlServer.js
@@ -21,11 +21,14 @@ const pubsub = new PubSub()
 
 dotenv.config()
 
-app.use(express.static(path.join(__dirname, './client/build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 const LunchServer = async () => {
   const httpServer = createServer(app)
   const schema = makeExecutableSchema({
